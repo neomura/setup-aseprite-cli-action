3b6df96f1d982cc34f969b7d4a5ae863c08de712
jameswilddev <jameswilddev@outlook.com>
2020-12-15 14:10:21 +0000

    Install xorg-dev.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 #sudo apt-get update
 #sudo apt-get install libx11-dev libxcursor-dev xorg-dev ninja-build -y
-sudo apt-get install ninja-build
+sudo apt-get install ninja-build xorg-dev
 cmake -E make_directory build
 cmake -E chdir build cmake -G "Ninja" -DENABLE_UI=OFF ../submodules/aseprite/aseprite
 cd build
