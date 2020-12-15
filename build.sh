b2b152af75dc9ed484a01e3a456e98fa3fb31790
jameswilddev <jameswilddev@outlook.com>
2020-12-15 14:27:12 +0000

    Only install dependencies on Linux.

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 set -e
-sudo apt-get update
-sudo apt-get install ninja-build xorg-dev
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  sudo apt-get update
+  sudo apt-get install ninja-build xorg-dev
+fi
+
 cmake -E make_directory build
 cmake -E chdir build cmake -G Ninja -DENABLE_UI=OFF ../submodules/aseprite/aseprite
 cd build
