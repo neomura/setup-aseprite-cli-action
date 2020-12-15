d0727dd291b7050e46572f2416a425f10806bf1f
jameswilddev <jameswilddev@outlook.com>
2020-12-15 14:12:20 +0000

    Unquote.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -e
 #sudo apt-get install libx11-dev libxcursor-dev xorg-dev ninja-build -y
 sudo apt-get install ninja-build xorg-dev
 cmake -E make_directory build
-cmake -E chdir build cmake -G "Ninja" -DENABLE_UI=OFF ../submodules/aseprite/aseprite
+cmake -E chdir build cmake -G Ninja -DENABLE_UI=OFF ../submodules/aseprite/aseprite
 cd build
 ninja
