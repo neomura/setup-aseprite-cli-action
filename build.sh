0b4a49bffea75aacb850468e86f683a18d52b3a3
jameswilddev <jameswilddev@outlook.com>
2020-12-15 14:07:18 +0000

    Install Ninja.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -e
 #sudo apt-get update
 #sudo apt-get install libx11-dev libxcursor-dev xorg-dev ninja-build -y
+sudo apt-get install ninja-build
 cmake -E make_directory build
 cmake -E chdir build cmake -G "Ninja" -DENABLE_UI=OFF ../submodules/aseprite/aseprite
 cd build
