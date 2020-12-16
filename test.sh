1b65375a46b14ea32353f7ef5c434d0cfb50640f
jameswilddev <jameswilddev@outlook.com>
2020-12-16 21:46:09 +0000

    Don't use BATS as it seems to have compatibility problems.

--- test.sh
+++ test.sh
@@ -1,17 +1,10 @@
-#!submodules/bats-core/bats-core/bin/bats
+set -e
 
-load "./submodules/ztombol/bats-support/load"
-load "./submodules/ztombol/bats-assert/load"
+rm -rf ./test/actual
 
-@test "makes the aseprite executable accessible to scripts" {
-  run rm -r ./test/actual
+aseprite --batch --list-tags --trim ./test/example.ase --data ./test/actual/example.json --save-as ./test/actual/example.png
 
-  run aseprite --batch --list-tags --trim ./test/example.ase --data ./test/actual/example.json --save-as ./test/actual/example.png
-  assert_success
-
-  run ls ./test/actual
-  assert_success
-  assert_output "example.json
+cmp <(ls ./test/actual) <(echo "example.json
 example1.png
 example2.png
 example3.png
@@ -20,108 +13,44 @@ example5.png
 example6.png
 example7.png
 example8.png
-example9.png"
+example9.png")
 
-  if [ "$(uname)" == "Darwin" ]; then
-    run brew install imagemagick jq
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    run sudo apt-get install imagemagick jq
-  else
-    run choco install imagemagick jq
-  fi
+if [ "$(uname)" == "Darwin" ]; then
+  brew install imagemagick jq
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  sudo apt-get install imagemagick jq
+else
+  choco install imagemagick jq
+fi
 
-  assert_success
+cmp <(identify -format "%wx%h" ./test/actual/example1.png) <(echo "48x32")
+cmp <(identify -format "%wx%h" ./test/actual/example2.png) <(echo "48x32")
+cmp <(identify -format "%wx%h" ./test/actual/example3.png) <(echo "48x32")
+cmp <(identify -format "%wx%h" ./test/actual/example4.png) <(echo "48x32")
+cmp <(identify -format "%wx%h" ./test/actual/example5.png) <(echo "48x32")
+cmp <(identify -format "%wx%h" ./test/actual/example6.png) <(echo "48x32")
+cmp <(identify -format "%wx%h" ./test/actual/example7.png) <(echo "48x32")
+cmp <(identify -format "%wx%h" ./test/actual/example8.png) <(echo "48x32")
+cmp <(identify -format "%wx%h" ./test/actual/example9.png) <(echo "48x32")
 
-  run identify -format "%wx%h" ./test/actual/example1.png
-  assert_success
-  assert_output "48x32"
+convert ./test/actual/example1.png ./test/actual/example1.rgba
+convert ./test/actual/example2.png ./test/actual/example2.rgba
+convert ./test/actual/example3.png ./test/actual/example3.rgba
+convert ./test/actual/example4.png ./test/actual/example4.rgba
+convert ./test/actual/example5.png ./test/actual/example5.rgba
+convert ./test/actual/example6.png ./test/actual/example6.rgba
+convert ./test/actual/example7.png ./test/actual/example7.rgba
+convert ./test/actual/example8.png ./test/actual/example8.rgba
+convert ./test/actual/example9.png ./test/actual/example9.rgba
 
-  run identify -format "%wx%h" ./test/actual/example2.png
-  assert_success
-  assert_output "48x32"
+cmp ./test/expected/example1.rgba ./test/actual/example1.rgba
+cmp ./test/expected/example2.rgba ./test/actual/example2.rgba
+cmp ./test/expected/example3.rgba ./test/actual/example3.rgba
+cmp ./test/expected/example4.rgba ./test/actual/example4.rgba
+cmp ./test/expected/example5.rgba ./test/actual/example5.rgba
+cmp ./test/expected/example6.rgba ./test/actual/example6.rgba
+cmp ./test/expected/example7.rgba ./test/actual/example7.rgba
+cmp ./test/expected/example8.rgba ./test/actual/example8.rgba
+cmp ./test/expected/example9.rgba ./test/actual/example9.rgba
 
-  run identify -format "%wx%h" ./test/actual/example3.png
-  assert_success
-  assert_output "48x32"
-
-  run identify -format "%wx%h" ./test/actual/example4.png
-  assert_success
-  assert_output "48x32"
-
-  run identify -format "%wx%h" ./test/actual/example5.png
-  assert_success
-  assert_output "48x32"
-
-  run identify -format "%wx%h" ./test/actual/example6.png
-  assert_success
-  assert_output "48x32"
-
-  run identify -format "%wx%h" ./test/actual/example7.png
-  assert_success
-  assert_output "48x32"
-
-  run identify -format "%wx%h" ./test/actual/example8.png
-  assert_success
-  assert_output "48x32"
-
-  run identify -format "%wx%h" ./test/actual/example9.png
-  assert_success
-  assert_output "48x32"
-
-  run convert ./test/actual/example1.png ./test/actual/example1.rgba
-  assert_success
-
-  run convert ./test/actual/example2.png ./test/actual/example2.rgba
-  assert_success
-
-  run convert ./test/actual/example3.png ./test/actual/example3.rgba
-  assert_success
-
-  run convert ./test/actual/example4.png ./test/actual/example4.rgba
-  assert_success
-
-  run convert ./test/actual/example5.png ./test/actual/example5.rgba
-  assert_success
-
-  run convert ./test/actual/example6.png ./test/actual/example6.rgba
-  assert_success
-
-  run convert ./test/actual/example7.png ./test/actual/example7.rgba
-  assert_success
-
-  run convert ./test/actual/example8.png ./test/actual/example8.rgba
-  assert_success
-
-  run convert ./test/actual/example9.png ./test/actual/example9.rgba
-  assert_success
-
-  run cmp ./test/expected/example1.rgba ./test/actual/example1.rgba
-  assert_success
-
-  run cmp ./test/expected/example2.rgba ./test/actual/example2.rgba
-  assert_success
-
-  run cmp ./test/expected/example3.rgba ./test/actual/example3.rgba
-  assert_success
-
-  run cmp ./test/expected/example4.rgba ./test/actual/example4.rgba
-  assert_success
-
-  run cmp ./test/expected/example5.rgba ./test/actual/example5.rgba
-  assert_success
-
-  run cmp ./test/expected/example6.rgba ./test/actual/example6.rgba
-  assert_success
-
-  run cmp ./test/expected/example7.rgba ./test/actual/example7.rgba
-  assert_success
-
-  run cmp ./test/expected/example8.rgba ./test/actual/example8.rgba
-  assert_success
-
-  run cmp ./test/expected/example9.rgba ./test/actual/example9.rgba
-  assert_success
-
-  run cmp <(jq -cS . ./test/expected/example.json) <(jq -cS . ./test/actual/example.json)
-  assert_success
-}
+cmp <(jq -cS . ./test/expected/example.json) <(jq -cS . ./test/actual/example.json)
