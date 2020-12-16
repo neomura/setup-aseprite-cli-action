93e4d367ae10bba251c8649e2c263f2f07f2aeed
jameswilddev <jameswilddev@outlook.com>
2020-12-16 18:22:29 +0000

    Fix tests when released.

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@ set -e
 # Unfortunately, the GitHub Actions Marketplace strips out all Git details
 # (including submodules) on publish, so we have to re-clone our own repository
 # to get the Aseprite submodule we plan to build.
-git clone https://github.com/$NEOMURA_SETUP_ASEPRITE_CLI_ACTION_REPOSITORY --branch ${NEOMURA_SETUP_ASEPRITE_CLI_ACTION_REF#refs/heads/} --recurse-submodules --depth 1 clone
+
+NEOMURA_SETUP_ASEPRITE_CLI_ACTION_BRANCH=${NEOMURA_SETUP_ASEPRITE_CLI_ACTION_REF#refs/heads/}
+NEOMURA_SETUP_ASEPRITE_CLI_ACTION_BRANCH=${NEOMURA_SETUP_ASEPRITE_CLI_ACTION_BRANCH#refs/tags/}
+
+git clone https://github.com/$NEOMURA_SETUP_ASEPRITE_CLI_ACTION_REPOSITORY --branch $NEOMURA_SETUP_ASEPRITE_CLI_ACTION_BRANCH --recurse-submodules --depth 1 clone
 
 if [ "$(uname)" == "Darwin" ]; then
   brew install ninja
