91f76a4aead116596db31a9ec63320cc57c5fc75
jameswilddev <jameswilddev@outlook.com>
2020-12-18 18:57:52 +0000

    Attempt to fix Windows tests.

--- test.sh
+++ test.sh
@@ -16,35 +16,38 @@ example8.png
 example9.png")
 
 if [ "$(uname)" == "Darwin" ]; then
+  MAGICK=magick
   brew install imagemagick
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  MAGICK=magick
   git clone https://github.com/SoftCreatR/imei
   cd imei
   sudo ./imei.sh
   cd ..
 else
+  MAGICK=magick.exe
   choco install imagemagick
 fi
 
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example1.png) <(echo "48x32")
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example2.png) <(echo "48x32")
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example3.png) <(echo "48x32")
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example4.png) <(echo "48x32")
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example5.png) <(echo "48x32")
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example6.png) <(echo "48x32")
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example7.png) <(echo "48x32")
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example8.png) <(echo "48x32")
-cmp <(magick identify -format "%wx%h\n" ./test/actual/example9.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example1.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example2.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example3.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example4.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example5.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example6.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example7.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example8.png) <(echo "48x32")
+cmp <($MAGICK identify -format "%wx%h\n" ./test/actual/example9.png) <(echo "48x32")
 
-magick convert ./test/actual/example1.png ./test/actual/example1.rgba
-magick convert ./test/actual/example2.png ./test/actual/example2.rgba
-magick convert ./test/actual/example3.png ./test/actual/example3.rgba
-magick convert ./test/actual/example4.png ./test/actual/example4.rgba
-magick convert ./test/actual/example5.png ./test/actual/example5.rgba
-magick convert ./test/actual/example6.png ./test/actual/example6.rgba
-magick convert ./test/actual/example7.png ./test/actual/example7.rgba
-magick convert ./test/actual/example8.png ./test/actual/example8.rgba
-magick convert ./test/actual/example9.png ./test/actual/example9.rgba
+$MAGICK convert ./test/actual/example1.png ./test/actual/example1.rgba
+$MAGICK convert ./test/actual/example2.png ./test/actual/example2.rgba
+$MAGICK convert ./test/actual/example3.png ./test/actual/example3.rgba
+$MAGICK convert ./test/actual/example4.png ./test/actual/example4.rgba
+$MAGICK convert ./test/actual/example5.png ./test/actual/example5.rgba
+$MAGICK convert ./test/actual/example6.png ./test/actual/example6.rgba
+$MAGICK convert ./test/actual/example7.png ./test/actual/example7.rgba
+$MAGICK convert ./test/actual/example8.png ./test/actual/example8.rgba
+$MAGICK convert ./test/actual/example9.png ./test/actual/example9.rgba
 
 cmp ./test/expected/example1.rgba ./test/actual/example1.rgba
 cmp ./test/expected/example2.rgba ./test/actual/example2.rgba
