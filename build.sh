cec81a109eeb7339ee506bd95fc8a9c58b93780c
jameswilddev <jameswilddev@outlook.com>
2021-01-29 22:20:32 +0000

    Attempt to fix nonfunctional PR tests.

--- build.sh
+++ build.sh
@@ -4,10 +4,7 @@ set -e
 # (including submodules) on publish, so we have to re-clone our own repository
 # to get the Aseprite submodule we plan to build.
 
-NEOMURA_SETUP_ASEPRITE_CLI_ACTION_BRANCH=${NEOMURA_SETUP_ASEPRITE_CLI_ACTION_REF#refs/heads/}
-NEOMURA_SETUP_ASEPRITE_CLI_ACTION_BRANCH=${NEOMURA_SETUP_ASEPRITE_CLI_ACTION_BRANCH#refs/tags/}
-
-git clone https://github.com/$NEOMURA_SETUP_ASEPRITE_CLI_ACTION_REPOSITORY --branch $NEOMURA_SETUP_ASEPRITE_CLI_ACTION_BRANCH --depth 1 clone
+git clone --config remote.origin.fetch=+$NEOMURA_SETUP_ASEPRITE_CLI_ACTION_REF https://github.com/$NEOMURA_SETUP_ASEPRITE_CLI_ACTION_REPOSITORY --depth 1 clone
 
 cd clone
 git submodule update --init --recursive submodules/aseprite/aseprite
