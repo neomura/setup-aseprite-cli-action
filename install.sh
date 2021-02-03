c0512a33d632afae2edcdc2ef6404884e3a61841
jameswilddev <jameswilddev@outlook.com>
2021-02-03 19:03:52 +0000

    Attempt to fix random Ubuntu regression.

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 set -e
 
-echo "$PWD/build/bin" >> $GITHUB_PATH
+if [ "$(uname)" == "Darwin" ]; then
+  echo "$PWD/build/bin" >> $GITHUB_PATH
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  sudo echo "$PWD/build/bin" >> $GITHUB_PATH
+else
+  echo "$PWD/build/bin" >> $GITHUB_PATH
+fi
