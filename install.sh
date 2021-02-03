e23eb5839551fc435b3b2523af89f6183115b5d5
jameswilddev <jameswilddev@outlook.com>
2021-02-03 19:34:25 +0000

    Remove nonfunctional workaround.

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 set -e
 
-if [ "$(uname)" == "Darwin" ]; then
-  echo "$PWD/build/bin" >> $GITHUB_PATH
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  sudo echo "$PWD/build/bin" >> $GITHUB_PATH
-else
-  echo "$PWD/build/bin" >> $GITHUB_PATH
-fi
+echo "$PWD/build/bin" >> $GITHUB_PATH
