8c79fa12ea9c4812026dc11e7e158057268e9923
jameswilddev <jameswilddev@outlook.com>
2026-06-06 20:15:49 +0100

    Remove debug logic.

--- install.sh
+++ install.sh
@@ -1,16 +1,3 @@
 set -e
 
-echo 1
-ls clone
-echo 2
-ls clone/submodules
-echo 3
-ls clone/submodules/aseprite
-echo 4
-ls clone/submodules/aseprite/aseprite
-echo 5
-ls clone/submodules/aseprite/aseprite/build
-echo 6
-ls clone/submodules/aseprite/aseprite/build/bin
-
 echo "$PWD/clone/submodules/aseprite/aseprite/build/bin" >> $GITHUB_PATH
