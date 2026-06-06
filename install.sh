286dd54917ee21a290ff0796e78a37396a10f5d5
jameswilddev <jameswilddev@outlook.com>
2026-06-06 19:40:20 +0100

    Debug.

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 set -e
 
+ls clone
+ls clone/submodules
+ls clone/submodules/aseprite
+ls clone/submodules/aseprite/aseprite
+ls clone/submodules/aseprite/aseprite/build
+ls clone/submodules/aseprite/aseprite/build/bin
+
 echo "$PWD/clone/submodules/aseprite/aseprite/build/bin" >> $GITHUB_PATH
