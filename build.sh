d7a1eb21384f2897cc5ebc67fc03e2c77f98e924
jameswilddev <jameswilddev@outlook.com>
2026-06-06 19:50:50 +0100

    Another missing dependency.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ fi
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get update
-  sudo apt-get install libxcb1-dev libxcursor-dev
+  sudo apt-get install libxcb1-dev libxcursor-dev libxi-dev
 fi
 
 cd clone
