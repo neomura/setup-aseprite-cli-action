9996039833314f950e561c43d9213e09e5310428
jameswilddev <jameswilddev@outlook.com>
2026-06-06 19:19:14 +0100

    Looks like there's now a build script.  Let's see.

--- build.sh
+++ build.sh
@@ -9,25 +9,5 @@ cd clone
 git checkout temp
 
 git submodule update --init --recursive --depth 1 submodules/aseprite/aseprite
-cd ..
-
-if [ "$(uname)" == "Darwin" ]; then
-  brew install ninja
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  sudo apt-get update
-  sudo apt-get install ninja-build xorg-dev libturbojpeg0-dev
-else
-  choco install ninja
-fi
-
-cmake -E make_directory build
-cmake -E chdir build cmake -G Ninja -DENABLE_UI=OFF ../clone/submodules/aseprite/aseprite
-cd build
-
-if [ "$(uname)" == "Darwin" ]; then
-  ninja
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  ninja
-else
-  ninja -j 1
-fi
+cd submodules/aseprite/aseprite
+bash ./build.sh
